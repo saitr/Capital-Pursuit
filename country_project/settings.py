0b0280bef02c1b17436b6d3a3eb28a234b2aa349
saitr <64057564+saitr@users.noreply.github.com>
2023-10-26 14:21:48 +0530

    Update settings.py

--- country_project/settings.py
+++ country_project/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'saitreddy06@gmail.com'
-EMAIL_HOST_PASSWORD = 'rndmsnlspighpxjj'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Default primary key field type
